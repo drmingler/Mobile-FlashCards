431f7a51caebbd8d068f3c55158514240704e5fa
“Drmingler” <kayymedia@gmail.com>
2020-04-09 13:52:12 +0200

    feat : Add Answer functional  component

---
+++ src/component/Answer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+// Answer Component renders the the answer
+export default function Answers({ answer }) {
+  return (
+    <View>
+      <Text>My name is Emmanuel</Text>
+    </View>
+  );
+}
